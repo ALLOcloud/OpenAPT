bcd7167932af9461726fcc0b4a46768ac998b157
Hubert LOBIT <hubert.lobit@allocloud.com>
2019-11-04 12:02:26 +0100

    Remove references to allocloud (binary, package)
    Spent: @30min
    Related-to: #35842

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='allocloud.openapt',
+    name='openapt',
     version='0.1.0',
     description='Specify and automate APT repository management.',
     author='Raphael Medaer',
@@ -30,7 +30,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'allocloud.openapt = allocloud.openapt.__main__:main',
+            'openapt = allocloud.openapt.__main__:main',
         ]
     },
 )
