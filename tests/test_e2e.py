42274022ce9173300c364abcb83312d202df9425
Hubert LOBIT <hubert.lobit@allocloud.com>
2019-11-20 09:32:38 +0100

    Make test setup configurable
    (1) drop support of dry-run mode in tests
    (2) add ability to add a `setup.json` file for cases
    
    Spent: @30min
    Related-to: #35842

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -2,13 +2,12 @@ import unittest.mock
 import logging
 import tempfile
 import pkgutil
-import shutil
 import os
 import io
 import json
 from pathlib import Path
 from dataclasses import dataclass
-from typing import Mapping
+from typing import List, Mapping
 
 import pytest
 
@@ -17,9 +16,10 @@ from allocloud.openapt import create_stream_handler, setup_logging, run
 
 @dataclass
 class Case:
+    setup: List[List[str]]
     input_path: str
-    expected_output: str
     options: Mapping[str, str]
+    expected_output: str
 
 def list_dirs(basedir):
     for subdir, dirs, _ in os.walk(basedir):
@@ -31,7 +31,13 @@ def cases(basedir):
     _cases = []
     for _dir in list_dirs(basedir):
         _case_dir = basedir / _dir
+        _setup_path = _case_dir / 'setup.json'
         _input_path = _case_dir / 'input.json'
+
+        _setup = []
+        if _setup_path.exists():
+            with open(_setup_path, 'r') as file:
+                _setup = json.load(file)
 
         for __dir in list_dirs(_case_dir):
             _options_path = _case_dir / __dir / 'options.json'
@@ -45,12 +51,16 @@ def cases(basedir):
             with open(_output_path, 'r') as file:
                 _output = file.read()
 
-            _case = Case(input_path=str(_input_path), expected_output=_output, options=_options)
+            _case = Case(
+                setup=_setup,
+                input_path=str(_input_path),
+                options=_options,
+                expected_output=_output,
+            )
             _cases.append(_case)
     return _cases
 
 CASES = cases(Path(__file__).parent / 'e2e' / 'cases')
-TEST_PACKAGE = Path(__file__).parent / 'e2e' / 'allocloud-test_1.0_all.deb'
 APTLY_CONF_TEMPLATE = json.loads(pkgutil.get_data('e2e', 'aptly.conf'))
 
 @pytest.mark.parametrize('case', CASES)
@@ -58,25 +68,16 @@ def test_e2e(case, caplog):
     setup_logging()
     caplog.set_level(logging.DEBUG)
 
-    def wrap_context_execute(func):
-        def wrapped(_args, *args, **kwargs):
-            if _args[:2] == ['repo', 'show']:
-                return False
-            return func(_args, *args, **kwargs)
-        return wrapped
-
     with tempfile.TemporaryDirectory() as root_dir:
         with open(Path(root_dir) / 'aptly.conf', 'w+') as f:
             f.write(json.dumps({**APTLY_CONF_TEMPLATE, 'rootDir': root_dir}))
 
         context = Context(
             config=f.name,
-            dry_run=shutil.which('aptly') is None,
             formats={
                 'snapshot': case.options.get('--snapshot-subst', '{name}'),
             },
         )
-        context.execute = wrap_context_execute(context.execute)
         formatter = NameFormatter()
         case.expected_output = formatter.format(
             case.expected_output,
@@ -84,9 +85,8 @@ def test_e2e(case, caplog):
             random=context.random,
         )
 
-        if not context.dry_run:
-            assert context.execute(['repo', 'create', 'allocloud'], 0)
-            assert context.execute(['repo', 'add', 'allocloud', str(TEST_PACKAGE)], 0)
+        for step in case.setup:
+            assert context.execute(step, 0)
 
         with unittest.mock.patch('allocloud.openapt.Context') as MockContext:
             MockContext.return_value = context
